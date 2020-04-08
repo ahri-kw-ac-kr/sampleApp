c786475c83f56b92ed6f61d62558198e2e285c7d
Ji2z <leegin98@naver.com>
2020-04-08 22:31:31 +0900

    뷰모델 작성

--- app/src/main/java/local/ahri/resttest/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/local/ahri/resttest/viewmodel/MainActivityViewModel.java
@@ -24,6 +24,7 @@ public class MainActivityViewModel extends ViewModel {
         return restfulAPIService.postAuth(user);
     }
 
+    /************************ User ***********************/
     public Single<PageDTO<UserDTO>> getAllUser() {
         return restfulAPIService.getAllUser();
     }
